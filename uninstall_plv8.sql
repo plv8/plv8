d55d811acc9da023f02257883a4e3d9de6f44b8a
Hitoshi Harada <umi.tanuki@gmail.com>
2012-12-08 22:11:02 -0800

    Update Changes file.

--- uninstall_plv8.sql
+++ uninstall_plv8.sql
@@ -4,3 +4,8 @@ DROP LANGUAGE plv8;
 DROP FUNCTION plv8_call_handler();
 DROP FUNCTION plv8_inline_handler(internal);
 DROP FUNCTION plv8_call_validator(oid);
+
+DROP DOMAIN plv8_int2array;
+DROP DOMAIN plv8_int4array;
+DROP DOMAIN plv8_float4array;
+DROP DOMAIN plv8_float8array;

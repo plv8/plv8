17150a4c10b3bdbf59aab9f2b013fe765e0967f5
Andrew Dunstan <andrew@dunslane.net>
2015-02-03 16:31:27 -0500

    a couple of improvements to package.sh

--- package.sh
+++ package.sh
@@ -10,6 +10,10 @@ fi
 TAG=$1
 
 cd $V8DIR
+
+GITHEAD=`git symbolic-ref --short HEAD`
+echo GITHEAD = $GITHEAD
+
 git checkout $TAG
 
 if [ $? -ne 0 ]; then
@@ -17,8 +21,19 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-V8VER=`v8 -e 'print(JSON.parse(read("META.json")).version)'`
+V8VER=`v8 -e 'print(JSON.parse(read("META.json")).version)' 2>/dev/null`
+if [ -z "$V8VER" ] ; then
+    V8VER=`d8 -e 'print(JSON.parse(read("META.json")).version)'`
+fi
+if [ -z "$V8VER" ] ; then
+    echo "Could not get version for plv8"
+    exit 1
+fi
+
 git archive --output $ORIG/plv8-$V8VER.zip \
             --prefix=plv8-$V8VER/ \
             --format=zip \
             $TAG
+
+git checkout $GITHEAD
+
